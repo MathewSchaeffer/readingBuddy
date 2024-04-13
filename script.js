6c9c9c153dfb1eb44171e99444f72c5692507f91
Mathew Schaeffer <mathew.schaeffer@outlook.com>
2024-04-13 16:11:08 +1000

    removed unused code

--- script.js
+++ script.js
@@ -6,30 +6,28 @@ let inputTitle = document.getElementById("title");
 let inputAuthor = document.getElementById("author");
 const myLibrary = [];
 
-// constructor
-
 function Book(author, title) {
     this.author = author;
     this.title = title;
 }
 
-myLibrary.push(newBook = new Book("Steven", "Jurassic Dorks"));
-myLibrary.push(newBook = new Book("Frank", "Jurassic Dorks II: The unofficial Autobiography of Automomous Jane"));
-
-
 function updateList() {
     const shelf = document.getElementById("shelf");
     shelf.replaceChildren();
     for (books of myLibrary) {
-        newLi = document.createElement("li");
+        let newLi = document.createElement("li");
         newLi.innerText = `${books.author}, ${books.title}`;
         shelf.append(newLi);
     }
 }
 
 // dummy data
+myLibrary.push(newBook = new Book("Steven", "Jurassic Dorks"));
+myLibrary.push(newBook = new Book("Frank", "Jurassic Dorks II: The unofficial Autobiography of Automomous Jane"));
 
 addBookButton.addEventListener("click", function (e) {
+    inputAuthor.value = "";
+    inputTitle.value = "";
     dialog.showModal();
 })
 
@@ -45,13 +43,8 @@ dialogButton.addEventListener("click", function (e) {
     } else {
         myLibrary.push(newBook = new Book(inputAuthor.value, inputTitle.value));
         updateList();
-        inputAuthor.value = "";
-        inputTitle.value = "";
-        // console.log(inputTitle.value);
-        // console.log(inputAuthor.value);
         dialog.close();
     }
-
 });
 
 dialogButtonCancel.addEventListener("click", function (e) {
