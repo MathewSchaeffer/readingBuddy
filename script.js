ff5ee778982c0f4ed0968cadae5727920fd04c27
Mathew Schaeffer <mathew.schaeffer@outlook.com>
2024-04-15 15:35:54 +1000

    added checkbox to flag if book is read

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const dialog = document.getElementById("dialogBox");
 const addBookButton = document.getElementById("addBook");
 let inputTitle = document.getElementById("title");
 let inputAuthor = document.getElementById("author");
+let checkBox = document.getElementById("checkBox");
 const myLibrary = [];
 
 function Book(author, title, read) {
@@ -18,34 +19,36 @@ function updateList() {
     for (let [index, books] of myLibrary.entries()) {
         let newLi = document.createElement("li");
         let newButton = document.createElement("button");
+        let newButtonText = ""
         newButton.innerText = `${books.read}`;
-        newLi.innerText = `${books.title}, ${books.author} - Read:  `;
+        newLi.innerText = `${books.title}, ${books.author}  `;
         shelf.append(newLi);
         newLi.append(newButton);
         newButton.classList.add("toggleButton");
         newButton.setAttribute("data-id", index);
-        console.dir(newButton.dataset.id);
+        // console.dir(newButton.dataset.id);
+        if (books.read) {
+
+            newButtonText = "Read";
+        } else {
+
+            newButtonText = "Not  Read ";
+        }
+        newButton.innerText = newButtonText;
         newButton.addEventListener("click", function (e) {
-            console.log(`Read Button pressed for ${books.title}`);
+            // console.log(`Read Button pressed for ${books.title}`);
             if (books.read) {
                 books.read = false;
+                newButtonText = "Not Read";
             } else {
                 books.read = true;
+                newButtonText = " Read ";
             }
-            newButton.innerText = books.read;
+            newButton.innerText = newButtonText;
             console.log(`${books.title} : Read: ${books.read}`)
         });
     }
-
 }
-
-// function updateButtonEventListeners() {
-//     const all = document.querySelectorAll(".toggleButton");
-//     for (elements of all) {
-//         elements.addEventListener("click",)
-//     }
-// }
-
 
 // dummy data
 myLibrary.push(newBook = new Book("Steven Hernio", "Jurassic Ducks", true));
@@ -55,11 +58,18 @@ myLibrary.push(newBook = new Book("Bigly Bob Gorbo", "The Art of Nuclear War", f
 addBookButton.addEventListener("click", function (e) {
     inputAuthor.value = "";
     inputTitle.value = "";
+    checkBox.checked = false;
     dialog.showModal();
 })
 
 dialogButton.addEventListener("click", function (e) {
     e.preventDefault();
+    let isRead = undefined
+    if (checkBox.checked) {
+        isRead = true;
+    } else {
+        isRead = false;
+    }
     if (inputTitle.value.length === 0 || inputAuthor.value.length === 0) {
         if (inputAuthor.value.length === 0) {
             console.log("Insert Author Name");
@@ -68,7 +78,7 @@ dialogButton.addEventListener("click", function (e) {
             console.log("Insert Title");
         }
     } else {
-        myLibrary.push(newBook = new Book(inputAuthor.value, inputTitle.value));
+        myLibrary.push(newBook = new Book(inputAuthor.value, inputTitle.value, isRead));
         updateList();
         dialog.close();
     }
