9289b6b604ecfbfe06967eb060ebd3d2478fd9d8
Mathew Schaeffer <mathew.schaeffer@outlook.com>
2024-04-15 13:20:02 +1000

    added toggle to show if book has been read

--- script.js
+++ script.js
@@ -6,24 +6,51 @@ let inputTitle = document.getElementById("title");
 let inputAuthor = document.getElementById("author");
 const myLibrary = [];
 
-function Book(author, title) {
+function Book(author, title, read) {
     this.author = author;
     this.title = title;
+    this.read = read;
 }
 
 function updateList() {
     const shelf = document.getElementById("shelf");
     shelf.replaceChildren();
-    for (books of myLibrary) {
+    for (let [index, books] of myLibrary.entries()) {
         let newLi = document.createElement("li");
-        newLi.innerText = `${books.author}, ${books.title}`;
+        let newButton = document.createElement("button");
+        newButton.innerText = `${books.read}`;
+        newLi.innerText = `${books.title}, ${books.author} - Read:  `;
         shelf.append(newLi);
+        newLi.append(newButton);
+        newButton.classList.add("toggleButton");
+        newButton.setAttribute("data-id", index);
+        console.dir(newButton.dataset.id);
+        newButton.addEventListener("click", function (e) {
+            console.log(`Read Button pressed for ${books.title}`);
+            if (books.read) {
+                books.read = false;
+            } else {
+                books.read = true;
+            }
+            newButton.innerText = books.read;
+            console.log(`${books.title} : Read: ${books.read}`)
+        });
     }
+
 }
 
+// function updateButtonEventListeners() {
+//     const all = document.querySelectorAll(".toggleButton");
+//     for (elements of all) {
+//         elements.addEventListener("click",)
+//     }
+// }
+
+
 // dummy data
-myLibrary.push(newBook = new Book("Steven", "Jurassic Dorks"));
-myLibrary.push(newBook = new Book("Frank", "Jurassic Dorks II: The unofficial Autobiography of Automomous Jane"));
+myLibrary.push(newBook = new Book("Steven Hernio", "Jurassic Ducks", true));
+myLibrary.push(newBook = new Book("Steven Hernio", "Jurassic Ducks II", false));
+myLibrary.push(newBook = new Book("Bigly Bob Gorbo", "The Art of Nuclear War", false))
 
 addBookButton.addEventListener("click", function (e) {
     inputAuthor.value = "";
